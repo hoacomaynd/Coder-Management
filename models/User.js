9f03ee2ad438b71a7c5f57d92886e9a69312c10b
MINH <minhdoan983@gmail.com>
2024-08-25 19:03:44 +0700

    a

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["employee", "manager"],
+    default: "employee"
+  },
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }]
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).send("Welcome to CoderSchool!")
+});
+const userRouter = require("./user.api");
+const taskRouter = require("./task.api");
+router.use("/users", userRouter);
+router.use("/task", taskRouter);
+module.exports = router;
